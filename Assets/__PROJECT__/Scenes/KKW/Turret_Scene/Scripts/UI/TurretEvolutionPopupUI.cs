de6ec9216cd34247fe88d5d05a8d893c12204193
snowmt98 <kimkyuwon1004@gmail.com>
2026-06-30 18:00:48 +0900

    레거시 정리

--- Assets/__PROJECT__/Scenes/KKW/Turret_Scene/Scripts/UI/TurretEvolutionPopupUI.cs
+++ Assets/__PROJECT__/Scenes/KKW/Turret_Scene/Scripts/UI/TurretEvolutionPopupUI.cs
@@ -293,10 +293,6 @@ public class TurretEvolutionPopupUI : TurretPopupPageUI
         {
             case RewardCurrencyType.Coin:
                 return "Coin";
-            case RewardCurrencyType.FirePart:
-                return "Fire";
-            case RewardCurrencyType.SpecialPart:
-                return "Special";
             default:
                 return currencyType.ToString();
         }

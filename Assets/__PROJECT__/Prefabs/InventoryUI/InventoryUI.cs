49a93eebc51dcef839ed2f0af6634da22e3f997d
TU-PJY <viska0319@icloud.com>
2026-07-07 10:56:40 +0900

    메인 메뉴 버튼 및 UI추가
    절전 모드 전환 버튼을 메인 메뉴로 이동
    
    절전 모드 전환 방식 변경

--- Assets/__PROJECT__/Prefabs/InventoryUI/InventoryUI.cs
+++ Assets/__PROJECT__/Prefabs/InventoryUI/InventoryUI.cs
@@ -151,8 +151,6 @@ public class InventoryUI : MonoBehaviour
     [Header("선택된 셀 색상")] public Color selectedCellColor;
     [Header("비활성화 버튼 색상")] public Color disableButtonColor;
 
-    [Header("절전 전환 버튼")] public GameObject powerSavingSwitchButton;
-
     [Header("자동 실행 진입 시간")] public float autoExecuteEnterTime;
     [Header("자동 실행 간격")] public float autoExecuteInterval;
 
@@ -434,7 +432,6 @@ public class InventoryUI : MonoBehaviour
     {
         mainController.SetActive(true);
         background.gameObject.SetActive(true);
-        powerSavingSwitchButton.SetActive(false);
         SetToInventoryTab();
         openState = true;
     }
@@ -446,7 +443,6 @@ public class InventoryUI : MonoBehaviour
     {
         mainController.SetActive(false);
         background.gameObject.SetActive(false);
-        powerSavingSwitchButton.SetActive(true);
         openState = false;
     }
 
